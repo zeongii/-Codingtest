4e0911a1d5e37bdaef9cf8e77248bc997be84912
zeongii <yaksanjy@naver.com>
2024-07-02 20:17:22 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        answer = 12000 * n + 2000*(k - n/10);
+        
+        
+        return answer;
+    }
+}
