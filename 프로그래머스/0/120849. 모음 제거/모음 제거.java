1f6132b6337e07d005a24141406c65f92493b5b2
zeongii <yaksanjy@naver.com>
2024-07-09 16:13:37 +0900

    [level 0] Title: 모음 제거, Time: 0.14 ms, Memory: 65.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        answer = my_string.replaceAll("[a,e,i,o,u]", "");
+        
+        return answer;
+    }
+}
