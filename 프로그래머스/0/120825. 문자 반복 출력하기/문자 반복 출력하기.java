40340d3d850e9ff6cc7681b9177fda83c80dfca1
zeongii <yaksanjy@naver.com>
2024-07-17 21:14:32 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 2.60 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] array = my_string.split("");
+        for (int i = 0; i < my_string.length(); i++){
+            answer += array[i].repeat(n);
+        }
+        return answer;
+    }
+}
