ed70abcc3f550e70c46cb69181a30ec683ff37c2
zeongii <yaksanjy@naver.com>
2024-11-16 11:06:09 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.09 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if (a%2 != 0 && b % 2 != 0) {
+            answer = (int)Math.pow(a,2) + (int)Math.pow(b,2);
+        } else if (a%2 == 0 && b % 2 == 0) {
+            int number = a - b;
+            answer = Math.abs(number);
+        } else {
+            answer = 2 * (a + b);
+        }
+        
+        
+        return answer;
+    }
+}
